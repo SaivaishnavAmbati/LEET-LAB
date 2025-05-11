48116d944af0cda663b9a8c0455cd697766d6ae9
SaivaishnavAmbati <ambatisaivaishnavprof@gmail.com>
2025-05-11 18:25:53 +0530

    auth, and problem formis added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problemRoutes.js";
 import executionRoute from "./routes/execute-code.route.js";
@@ -11,7 +11,12 @@ import playlistRoutes from "./routes/playlist.routes.js";
 dotenv.config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin:"http://localhost:5173",
+    credentials:true
+  })
+)
 app.use(express.json());
 app.use(cookieParser());
 
